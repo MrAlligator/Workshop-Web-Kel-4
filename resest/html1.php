ee6e587379bbf8c54a3601ef33644e4383c0cc65
Rizki Widya Pratama <rizkiw8778@gmail.com>
2019-10-02 13:01:46 +0700

    Update html1.php

--- resest/html1.php
+++ resest/html1.php
@@ -10,10 +10,49 @@
     <h2 style="color:blue">Nama Saya, Rizki Widya Pratama</h2>
     <h3>Politeknik Negeri Jember</h3>
     <button><a href="html2.html">Tugas 2</a></button><br /><br /><br /><br />
-</body>
+    <?php
+    echo "Changed To PHP </br></br>";
+    $tempat = "di Polije";
+    $Tempat = "di Polije Kampus Bondowoso";
+    $TempaT = "di Polije Kampus Impian";
 
-<?php
-echo "<h2>Convert To PHP</h2><br /><br />";
-?>
+    echo "<h1 style='color:red'>Selamat Belajar HTML " . $tempat . "</h1>";
+    echo "<h2 style='color:blue'>Selamat Belajar HTML</h2>";
+    echo "<h3>Selamat Belajar HTML " . $Tempat . "</h3>";
+    echo $TempaT . "</br></br>";
+    echo date("Y/m/d") . "</br></br>";
+
+    //Penjumlahan
+    $a = 10;
+    $b = $c = 20;
+    $d = 2;
+    $jumlah = ($a + $b) / $d;
+
+    echo $jumlah . "</br></br>";
+
+    for ($x = 0; $x <= 10; $x++) {
+        echo "Tempat Ke - " . $x . "<br>";
+    }
+
+    $t = date("H");
+
+    if ($t < "20") {
+        echo "Selamat Pagi!";
+    }
+
+    //Konstanta
+    define("Khafid", "Selamat datang di Polije!");
+    echo "</br></br>" . Khafid;
+
+    //Fungsi
+    function khafid()
+    {
+        echo "</br></br>" . Khafid . " Dua";
+    }
+
+    khafid();
+
+    ?>
+</body>
 
 </html>
