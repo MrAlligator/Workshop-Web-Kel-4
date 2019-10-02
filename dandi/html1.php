e184a043a4c749ec1e8344aa6c6d0ad5d95ecfa1
Ahmad Dandi Irawan <55469273+dandyIrawan@users.noreply.github.com>
2019-10-02 10:14:21 +0700

    php

---
+++ dandi/html1.php
@@ -0,0 +1,18 @@
+
+<html>
+
+<head>
+<title>belajar html</title>
+</head>
+
+<body>
+	<h1 style="color:red">selamat belajar HTML</h1>
+	<h1 style="color:yellow">AHMAD DANDI IRAWAN</h1>
+	<h3>polije kampus bondowoso</h3>
+
+	<?php
+echo "dandi";
+?>
+</body>
+
+</html>
