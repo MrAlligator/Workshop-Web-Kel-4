00e9a753ed2397f36d0b163d7dce6ec77618390f
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-10 16:49:40 -0700

    Update koneksi.php

--- resest/koneksi.php
+++ resest/koneksi.php
@@ -8,13 +8,3 @@ if (mysqli_connect_errno()){
 }
 
 ?>
-<?php 
- 
-$koneksi = mysqli_connect("127.0.0.1","root","","db_smk");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
