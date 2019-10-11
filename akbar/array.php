1807a1443348c8c0a6864cce24c541b4c02f13dd
Rizkyakbar-mhs <55370807+Rizkyakbar-mhs@users.noreply.github.com>
2019-10-11 10:33:13 +0700

    bkpm array,while,aritmatika,form,koneksi,include dan require,tampil data
    Tugas Bkpm 2

---
+++ akbar/array.php
@@ -0,0 +1,12 @@
+<?php 
+
+//penamaan isi array
+$gear['SS'] = "perfect";
+$gear['S'] = "lumayan perfect";
+$gear['A'] = "bagus";
+$gear['B'] = "lumayan bagus";
+
+// menampilkan isi array yang bernama jeruk
+echo $gear['SS'];
+
+?>
