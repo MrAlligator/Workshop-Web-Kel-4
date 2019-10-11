0cc7e2a949f801955d73eec66a5c59433cc246b0
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-10 20:29:14 -0700

    Create home.php

---
+++ resest/home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD PHP dan MySQLi</title>
+</head>
+<body>
+<h2>CRUD data Siswa</h2>
+	<br/>
+	<a href="tambah.php">+ TAMBAH MAHASISWA</a>
+	<br/>
+	<br/>
+	<table border="1">
+		<tr>
+			<th>NO</th>
+			<th>Nama</th>
+			<th>NIS</th>
+			<th>Jurusan</th>
+			<th>OPSI</th>
+		</tr>
+		<?php 
+		include 'koneksi.php';
+		$no = 1;
+		$data = mysqli_query($koneksi,"select * from siswa");
+		while($d = mysqli_fetch_array($data)){
+			?>
+			<tr>
+				<td><?php echo $no++; ?></td>
+				<td><?php echo $d['nama_siswa']; ?></td>
+				<td><?php echo $d['nis']; ?></td>
+				<td><?php echo $d['jurusan']; ?></td>
+				<td>
+					<a href="edit.php?id=<?php echo $d['id']; ?>">EDIT</a>
+					<a href="hapus.php?id=<?php echo $d['id']; ?>">HAPUS</a>
+				</td>
+			</tr>
+			<?php 
+		}
+		?>
+	</table>
+</body>
+</html>
