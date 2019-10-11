4113d10dde53a22fe12512986a096f99b3b6a24e
febreroaraya <febrero.fa@gmail.com>
2019-10-12 04:43:08 +0700

    Create tampil.php

---
+++ feb/tampil.php
@@ -0,0 +1,12 @@
+<?php 
+// menangkap data nama dengan method nama
+$team = $_GET['nama_team'];
+// menangkap data bracket dengan method nama
+$bracket = $_GET['bracket'];
+
+// menampilkan data nama
+echo "team anda : " . $team;
+echo "<br/>";
+// menampilkan data bracket
+echo "bracket anda : " . $bracket;
+?>
