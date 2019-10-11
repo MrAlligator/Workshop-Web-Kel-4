1de184112334e559c55da578f4cee527bb6ca9a1
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-10 19:55:10 -0700

    Create array.php

---
+++ resest/array.php
@@ -0,0 +1,9 @@
+<?php
+
+$procesor['core2duo'] = "Kecepatan 2.2 GHz";
+$procesor['corei3'] = "Kecepatan 2.26 GHz";
+$procesor['corei5'] = "Kecepatan 2.93 GHz";
+$procesor['corei7'] = "Kecepatan 3.33 GHz";
+
+echo $procesor['corei5'];
+?>
