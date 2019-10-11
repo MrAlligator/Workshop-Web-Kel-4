0dedae105376c9ab3adce4dd108485b0392acf58
febreroaraya <febrero.fa@gmail.com>
2019-10-12 04:37:34 +0700

    Create array.php

---
+++ feb/array.php
@@ -0,0 +1,12 @@
+<?php 
+
+//penamaan isi array
+$gear['S+'] = "sempurna";
+$gear['S'] = "sangat baik";
+$gear['A'] = "baik";
+$gear['B'] = "cukup";
+
+// menampilkan isi array yang bernama sempurna
+echo $gear['S+'];
+
+?>
