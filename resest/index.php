598e1c0f08fe8d287297934fd99c5fff0aa8e317
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-10 19:57:34 -0700

    Create index.php

---
+++ resest/index.php
@@ -0,0 +1,14 @@
+<form method="get" action="tampil.php">
+	<label>Masukkan Nama</label><br/>
+	<input type="text" name="nama"><br/>
+	<label>Masukkan NIM</label><br/>
+	<input type="text" name="nim"><br/>	
+	<input type="submit" value="oke">
+</form>
+
+
+<?php
+include('tes.php');
+
+echo "Belajar Include() dan Require() di www.malasngoding.com";
+?>
