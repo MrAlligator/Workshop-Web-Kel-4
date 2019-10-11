512d69a412b803dda31cdc58b68e891983639b78
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-10 19:55:57 -0700

    Create aritmatika.php

---
+++ resest/aritmatika.php
@@ -0,0 +1,21 @@
+<?php 
+$a = 2;
+$b = 25;
+$c = 4;
+
+echo "<br>";
+echo $a + $b; 
+echo "<br>";
+echo $b - $c; 
+echo "<br>";
+echo $c * $a * $b; 
+echo "<br>";
+echo $c / $a;
+echo "<br>";
+echo ($a * $c) - $a; 
+echo "<br>";
+echo $a * $c + $a * $b;
+echo "<br>";
+echo $b % $a;
+
+?>
