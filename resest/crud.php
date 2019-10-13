e9794e2ed8f2ec4670827ff1943841ca121a342a
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-13 10:34:49 -0700

    Try Login

--- resest/crud.php
+++ resest/crud.php
@@ -14,23 +14,35 @@
 	<br/>
 	<table border="1">
 		<tr>
-			<th>NO</th>
+			<th>No</th>
 			<th>Nama</th>
 			<th>NIS</th>
-			<th>Jurusan</th>
+			<th>Jenis Kelamin</th>
+			<th>Agama</th>
+			<th>Tempat Lahir</th>
+			<th>Tanggal Lahir</th>
+			<th>Alamat</th>
+			<th>No. Telepon</th>
+			<th>Password</th>
 			<th>OPSI</th>
 		</tr>
 		<?php 
 		include 'koneksi.php';
 		$no = 1;
-		$data = mysqli_query($koneksi,"select * from siswa");
+		$data = mysqli_query($koneksi,"select * from tb_siswa");
 		while($d = mysqli_fetch_array($data)){
 			?>
 			<tr>
 				<td><?php echo $no++; ?></td>
 				<td><?php echo $d['nama_siswa']; ?></td>
 				<td><?php echo $d['nis']; ?></td>
-				<td><?php echo $d['jurusan']; ?></td>
+				<td><?php echo $d['jk_siswa']; ?></td>
+				<td><?php echo $d['agama_siswa']; ?></td>
+				<td><?php echo $d['tmptlahir_siswa']; ?></td>
+				<td><?php echo $d['tgllahir_siswa']; ?></td>
+				<td><?php echo $d['alamat_siswa']; ?></td>
+				<td><?php echo $d['telp_siswa']; ?></td>
+				<td><?php echo $d['password']; ?></td>
 				<td>
 					<a href="edit.php?id=<?php echo $d['id']; ?>">EDIT</a>
 					<a href="hapus.php?id=<?php echo $d['id']; ?>">HAPUS</a>
---
+++ resest/multi_user/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Halaman Login</h1>
+
+<?php 
+if(isset($_GET['pesan'])){
+    if($_GET['pesan']=="gagal"){
+        echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+    }
+}
+?>
+
+<div class="kotak_login">
+    <p class="tulisan_login">Silahkan login</p>
+    <form action="cek_login.php" method="post">
+        <label>Username</label>
+        <input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+        <label>Password</label>
+        <input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+
+        <input type="submit" class="tombol_login" value="LOGIN">
+
+        <br/>
+        <br/>
+        <center>
+            <a class="link" href="https://www.malasngoding.com">kembali</a>
+        </center>
+    </form>
+    
+</div>
+</body>
+</html>
