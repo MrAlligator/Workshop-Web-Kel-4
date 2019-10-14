c1e9e52c6b68b4f687febe2e3e945aaca1b982f3
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-14 22:47:32 +0700

    Update tambah_aksi.php

--- resest/tambah_aksi.php
+++ resest/tambah_aksi.php
@@ -4,13 +4,19 @@ include 'koneksi.php';
 
 // menangkap data yang di kirim dari form
 $nama = $_POST['nama'];
-$nim = $_POST['nim'];
+$nis = $_POST['nis'];
+$jenisKelamin = $_POST['jenisKelamin'];
+$agama = $_POST['agama'];
+$tmptlahir = $_POST['tmptlahir'];
+$tanggal = $_POST['tanggal'];
 $alamat = $_POST['alamat'];
+$notelp = $_POST['notelp'];
+$passwd = $_POST['passwd'];
 
 // menginput data ke database
-mysqli_query($koneksi,"insert into tb_siswa values('','$nama','$nim','$alamat')");
+mysqli_query($koneksi,"insert into tb_siswa values('','$nama','$nis','$jenisKelamin','$agama','$tmptlahir','$tanggal','$alamat','$notelp','$passwd')");
 
 // mengalihkan halaman kembali ke index.php
-header("location:index.php");
+header("location:crud.php");
 
 ?>
