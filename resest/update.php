8f923046cac090fd8ff0ff78e844324288e5b70f
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-14 23:29:09 +0700

    Update update.php

--- resest/update.php
+++ resest/update.php
@@ -4,18 +4,18 @@ include 'koneksi.php';
 
 // menangkap data yang di kirim dari form
 $id = $_POST['id'];
-$nama = $_POST['nama'];
+$nama = $_POST['nama_siswa'];
 $nis = $_POST['nis'];
-$jenisKelamin = $_POST['jenisKelamin'];
-$agama = $_POST['agama'];
-$tmptlahir = $_POST['tmptlahir'];
-$tanggal = $_POST['tanggal'];
-$alamat = $_POST['alamat'];
-$notelp = $_POST['notelp'];
-$passwd = $_POST['passwd'];
+$jenisKelamin = $_POST['jk_siswa'];
+$agama = $_POST['agama_siswa'];
+$tmptlahir = $_POST['tmptlahir_siswa'];
+$tanggal = $_POST['tgllahir_siswa'];
+$alamat = $_POST['alamat_siswa'];
+$notelp = $_POST['telp_siswa'];
+$passwd = $_POST['password'];
 
 // update data ke database
-mysqli_query($koneksi,"update tb_siswa set nama='$nama', nis='$nis', jenisKelamin='$jenisKelamin', agama='$agama', tmptlahir='$tmptlahir', tanggal='$tanggal', alamat='$alamat' notelp='$notelp', passwd='$passwd', where id='$id'");
+mysqli_query($koneksi,"update tb_siswa set nama_siswa='$nama', nis='$nis', jk_siswa='$jenisKelamin', agama_siswa='$agama', tmptlahir_siswa='$tmptlahir', tgllahir_siswa='$tanggal', alamat_siswa='$alamat' telp_siswa='$notelp', password='$passwd', where id='$id'");
 
 // mengalihkan halaman kembali ke index.php
 header("location:crud.php");
