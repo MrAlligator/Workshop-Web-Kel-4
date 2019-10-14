abff00dacec309bb7d134e9c0d585d9c82ecbae7
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-14 22:19:48 +0700

    Create tambah.php

---
+++ resest/tambah.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h2>CRUD DATA MAHASISWA - WWW.MALASNGODING.COM</h2>
+	<br/>
+	<a href="index.php">KEMBALI</a>
+	<br/>
+	<br/>
+	<h3>TAMBAH DATA MAHASISWA</h3>
+	<form method="post" action="tambah_aksi.php">
+		<table>
+			<tr>			
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>NIM</td>
+				<td><input type="number" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="SIMPAN"></td>
+			</tr>		
+		</table>
+	</form>
+</body>
+</html>
