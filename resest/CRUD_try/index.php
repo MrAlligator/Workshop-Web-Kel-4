578618be9a779b3c19d0b4636d199cd2823cef6c
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-18 06:38:33 +0700

    Update index.php

--- resest/CRUD_try/index.php
+++ resest/CRUD_try/index.php
@@ -11,21 +11,31 @@
 		<tr>
 			<th>NO</th>
 			<th>Nama</th>
-			<th>NIM</th>
+			<th>NIS</th>
+			<th>Jenis Kelamin</th>
+			<th>Agama</th>
+			<th>Tempat Lahir</th>
+			<th>Tanggal Lahir</th>
 			<th>Alamat</th>
+			<th>No. Telp</th>
 			<th>OPSI</th>
 		</tr>
 		<?php 
 		include 'koneksi.php';
 		$no = 1;
-		$data = mysqli_query($koneksi,"select * from mahasiswa");
+		$data = mysqli_query($koneksi,"select * from tb_siswa");
 		while($d = mysqli_fetch_array($data)){
 			?>
 			<tr>
 				<td><?php echo $no++; ?></td>
 				<td><?php echo $d['nama']; ?></td>
-				<td><?php echo $d['nim']; ?></td>
+				<td><?php echo $d['nis']; ?></td>
+				<td><?php echo $d['jk']; ?></td>
+				<td><?php echo $d['agama']; ?></td>
+				<td><?php echo $d['tmptlahir']; ?></td>
+				<td><?php echo $d['tgllahir']; ?></td>
 				<td><?php echo $d['alamat']; ?></td>
+				<td><?php echo $d['notelp']; ?></td>
 				<td>
 					<button><a href="edit.php?id=<?php echo $d['id']; ?>">EDIT</a></button>
 					<button><a href="hapus.php?id=<?php echo $d['id']; ?>">HAPUS</a></button>
