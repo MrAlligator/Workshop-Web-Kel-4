1ae18d7904cf7e7b2b6154a4bee32a0a07c2cc40
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-18 06:38:37 +0700

    Update edit.php

--- resest/CRUD_try/edit.php
+++ resest/CRUD_try/edit.php
@@ -7,6 +7,43 @@
     <title>Edit Data</title>
 </head>
 <body>
-    
+<h3>EDIT DATA MAHASISWA</h3>
+
+<?php
+include 'koneksi.php';
+$id = $_GET['id'];
+$data = mysqli_query($koneksi,"select * from mahasiswa where id='$id'");
+while($d = mysqli_fetch_array($data)){
+    ?>
+    <form method="post" action="update.php">
+        <table>
+            <tr>			
+                <td>Nama</td>
+                <td>
+                    <input type="hidden" name="id" value="<?php echo $d['id']; ?>">
+                    <input type="text" name="nama" value="<?php echo $d['nama']; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td><input type="text" name="nim" value="<?php echo $d['nim']; ?>"></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td><input type="text" name="alamat" value="<?php echo $d['alamat']; ?>"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="SIMPAN"></td>
+            </tr>		
+        </table>
+    </form>
+    <?php 
+}
+?>
+</br>
+</br>
+</br>
+<button><a href="index.php">KEMBALI</a></button>
 </body>
 </html>
