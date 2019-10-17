4230768f3c6625ee27d929430f132669985c2903
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-18 06:38:28 +0700

    Update koneksi.php

--- resest/CRUD_try/koneksi.php
+++ resest/CRUD_try/koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("127.0.0.1","root","","coba");
+$koneksi = mysqli_connect("127.0.0.1","root","","db_smk");
 
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
