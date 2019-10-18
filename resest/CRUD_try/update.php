56e7bc34dcf39307e17559567c1819ed91cd789a
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-18 07:09:17 +0700

    Update update.php

--- resest/CRUD_try/update.php
+++ resest/CRUD_try/update.php
@@ -1,17 +1,18 @@
 <?php 
-// koneksi database
 include 'koneksi.php';
- 
-// menangkap data yang di kirim dari form
+
 $id = $_POST['id'];
 $nama = $_POST['nama'];
-$nim = $_POST['nim'];
+$nis = $_POST['nis'];
+$jk = $_POST['jk'];
+$agama = $_POST['agama'];
+$tmptlahir = $_POST['tmptlahir'];
+$tgllahir = $_POST['tgllahir'];
 $alamat = $_POST['alamat'];
- 
-// update data ke database
-mysqli_query($koneksi,"update mahasiswa set nama='$nama', nim='$nim', alamat='$alamat' where id='$id'");
- 
-// mengalihkan halaman kembali ke index.php
+$telp = $_POST['telp'];
+
+mysqli_query($koneksi,"update tb_siswa set nama='$nama', nis='$nis', jk='$jk', agama='$agama', tmptlahir='$tmptlahir', tgllahir='$tgllahir', alamat='$alamat', telp='$telp' where id='$id'");
+
 header("location:index.php");
- 
+
 ?>
