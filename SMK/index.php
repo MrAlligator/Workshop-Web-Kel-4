bd149e19c01f0beb6904b154f5684721e82408dc
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-10-22 03:09:54 +0700

    index

--- SMK/index.php
+++ SMK/index.php
@@ -506,81 +506,75 @@
               <div class="validation"></div>
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-              <div class="validation"></div>
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+            <div class="validation"></div>
             </div>
-          </div>
+        </div>
 
-          <div class="col-md-6 col-sm-6 col-xs-12 right">
+        <div class="col-md-6 col-sm-6 col-xs-12 right">
             <div class="form-group">
-              <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-              <div class="validation"></div>
+            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+            <div class="validation"></div>
             </div>
-          </div>
+        </div>
 
-          <div class="col-xs-12">
+        <div class="col-xs-12">
             <!-- Button -->
             <button type="submit" id="submit" name="submit" class="form contact-form-button light-form-button oswald light">SEND EMAIL</button>
-          </div>
+        </div>
         </form>
 
-      </div>
     </div>
-  </section>
-  <!--/ Contact-->
-  <!--Footer-->
-  <footer id="footer" class="footer">
+    </div>
+</section>
+<!--/ Contact-->
+<!--Footer-->
+<footer id="footer" class="footer">
     <div class="container text-center">
 
-      <h3>Start Your Free Trial Now!</h3>
+    <h3>Start Your Free Trial Now!</h3>
 
-      <form class="mc-trial row">
+    <form class="mc-trial row">
         <div class="form-group col-md-3 col-md-offset-2 col-sm-4">
-          <div class=" controls">
+        <div class=" controls">
             <input name="name" placeholder="Enter Your Name" class="form-control" type="text">
-          </div>
+        </div>
         </div>
         <!-- End email input -->
         <div class="form-group col-md-3 col-sm-4">
-          <div class=" controls">
+        <div class=" controls">
             <input name="EMAIL" placeholder="Enter Your email" class="form-control" type="email">
-          </div>
+        </div>
         </div>
         <!-- End email input -->
         <div class="col-md-2 col-sm-4">
-          <p>
+        <p>
             <button name="submit" type="submit" class="btn btn-block btn-submit">
             Submit <i class="fa fa-arrow-right"></i></button>
-          </p>
+        </p>
         </div>
-      </form>
-      <!-- End newsletter-form -->
-      <ul class="social-links">
+    </form>
+    <!-- End newsletter-form -->
+    <ul class="social-links">
         <li><a href="#link"><i class="fa fa-twitter fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-facebook fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-google-plus fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-dribbble fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-linkedin fa-fw"></i></a></li>
-      </ul>
-      ©2016 Mentor Theme. All rights reserved
-      <div class="credits">
-        <!--
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Mentor
-        -->
+    </ul>
+    ©2016 Mentor Theme. All rights reserved
+    <div class="credits">
         Designed by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
-      </div>
     </div>
-  </footer>
-  <!--/ Footer-->
+    </div>
+</footer>
+<!--/ Footer-->
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery.easing.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/custom.js"></script>
-  <script src="contactform/contactform.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="contactform/contactform.js"></script>
 
 </body>
 
