06f1dafeb281bffe28acc1cff4e2b871cbe165b6
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-04 10:34:18 +0700

    Update pramuka.php

--- SMK/pramuka.php
+++ SMK/pramuka.php
@@ -248,201 +248,7 @@
 	<!--================ End Registration Area =================-->
 
 	<!--================ Start Events Area =================-->
-	<div class="popular_courses lite_bg">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-6">
-					<div class="main_title">
-						<a href="#"><h2>Berita</h2></a>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#"><i class="lnr lnr-user"></i>355</a>
-									</span>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a>
-									</span>
-								</div>
-								<div>
-									<span class="price">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-user"></i>355
-										</a>
-									</span>
-									<span class="meta_info"><a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a></span>
-								</div>
-								<div>
-									<span class="price df_color1">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-user"></i>355
-										</a>
-									</span>
-									<span class="meta_info"><a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a></span>
-								</div>
-								<div>
-									<span class="price">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-user"></i>355
-										</a>
-									</span>
-									<span class="meta_info"><a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a></span>
-								</div>
-								<div>
-									<span class="price df_color2">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!--================ End Events Area =================-->
 
-    <!--================Contact Area =================-->
-	<section class="contact_area section_gap">
-        <div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-6">
-					<div class="main_title">
-						<a href="#"><h2>Hubungi Kami</h2></a>
-					</div>
-				</div>
-			</div>
-            <div id="mapBox" class="mapBox" data-lat="-7.924831" data-lon="113.879707" data-zoom="17"
-                data-mlat="-7.924831" data-mlon="113.879707">
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="contact_info">
-                        <div class="info_item">
-                            <i class="lnr lnr-home"></i>
-                            <h6>Bondowoso, Jawa Timur</h6>
-                            <p>Santa monica bullevard</p>
-                        </div>
-                    </div>
-				</div>
-				<div class="col-md-4">
-					<div class="contact_info">
-						<div class="info_item">
-    	                    <i class="lnr lnr-phone-handset"></i>
-        	                <h6>00 (440) 9865 562</h6>
-            	            <p>Mon to Fri 9am to 6 pm</p>
-                	    </div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="contact_info">
-						<div class="info_item">
-                            <i class="lnr lnr-envelope"></i>
-                            <h6>support@colorlib.com</a></h6>
-                            <p>Send us your query anytime!</p>
-                        </div>
-					</div>
-				</div>
-			</div>
-        </div>
-    </section>
     <!--================Contact Area =================-->
 
 	<!--================ Start footer Area  =================-->
@@ -461,9 +267,9 @@
 				<div class="col-lg-2 col-md-6 single-footer-widget">
 					<h4>Data Sekolah</h4>
 					<ul>
-						<li><a href="#">Siswa</a></li>
-						<li><a href="#">Guru</a></li>
-						<li><a href="#">Karyawan</a></li>
+						<li><a href="siswa.php">Siswa</a></li>
+						<li><a href="guru.php">Guru</a></li>
+						<li><a href="karyawan.php">Karyawan</a></li>
 					</ul>
 				</div>
 				<div class="col-lg-2 col-md-6 single-footer-widget">
@@ -476,7 +282,7 @@
 				<div class="col-lg-2 col-md-6 single-footer-widget">
 					<h4>Ekstrakulikuler</h4>
 					<ul>
-						<li><a href="#">Pramuka</a></li>
+						<li><a href="pramuka.php">Pramuka</a></li>
 						<li><a href="#">Pencak Silat</a></li>
 					</ul>
 				</div>
