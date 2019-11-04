8130237595de103a89209cbc4c8dc407156b4490
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-04 09:06:57 +0700

    Update siswa.php

--- SMK/siswa.php
+++ SMK/siswa.php
@@ -207,77 +207,59 @@
 
 	<!--================ Start footer Area  =================-->
     <footer class="footer-area section_gap">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Top Products</h4>
-						<ul>
-							<li><a href="#">Managed Website</a></li>
-							<li><a href="#">Manage Reputation</a></li>
-							<li><a href="#">Power Tools</a></li>
-							<li><a href="#">Marketing Service</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Quick Links</h4>
-						<ul>
-							<li><a href="#">Jobs</a></li>
-							<li><a href="#">Brand Assets</a></li>
-							<li><a href="#">Investor Relations</a></li>
-							<li><a href="#">Terms of Service</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Features</h4>
-						<ul>
-							<li><a href="#">Jobs</a></li>
-							<li><a href="#">Brand Assets</a></li>
-							<li><a href="#">Investor Relations</a></li>
-							<li><a href="#">Terms of Service</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Resources</h4>
-						<ul>
-							<li><a href="#">Guides</a></li>
-							<li><a href="#">Research</a></li>
-							<li><a href="#">Experts</a></li>
-							<li><a href="#">Agencies</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-4 col-md-6 single-footer-widget">
-						<h4>Newsletter</h4>
-						<p>You can trust us. we only send promo offers,</p>
-						<div class="form-wrap" id="mc_embed_signup">
-							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-								method="get" class="form-inline">
-								<input class="form-control" name="EMAIL" placeholder="Your Email Address" onfocus="this.placeholder = ''"
-									onblur="this.placeholder = 'Your Email Address '" required="" type="email">
-								<button class="click-btn btn btn-default">
-									<span>subscribe</span>
-								</button>
-								<div style="position: absolute; left: -5000px;">
-									<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-								</div>
-	
-								<div class="info"></div>
-							</form>
-						</div>
-					</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-6 single-footer-widget">
+					<h4>Profil Sekolah</h4>
+					<ul>
+						<li><a href="#">Sejarah</a></li>
+						<li><a href="#">Profil Singkat</a></li>
+						<li><a href="#">Visi dan Misi</a></li>
+						<li><a href="#">Struktur</a></li>
+					</ul>
 				</div>
-				<div class="row footer-bottom d-flex justify-content-between">
-					<p class="col-lg-8 col-sm-12 footer-text m-0 text-white">Copyright © 2018 All rights reserved | This
-						template is
-						made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a></p>
-					<div class="col-lg-4 col-sm-12 footer-social">
-						<a href="#"><i class="fa fa-facebook"></i></a>
-						<a href="#"><i class="fa fa-twitter"></i></a>
-						<a href="#"><i class="fa fa-dribbble"></i></a>
-						<a href="#"><i class="fa fa-behance"></i></a>
-					</div>
+				<div class="col-lg-2 col-md-6 single-footer-widget">
+					<h4>Data Sekolah</h4>
+					<ul>
+						<li><a href="siswa.php">Siswa</a></li>
+						<li><a href="guru.php">Guru</a></li>
+						<li><a href="karyawan.php">Karyawan</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-2 col-md-6 single-footer-widget">
+					<h4>Jurusan</h4>
+					<ul>
+						<li><a href="#">Multimedia</a></li>
+						<li><a href="#">Pemensinan</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-2 col-md-6 single-footer-widget">
+					<h4>Ekstrakulikuler</h4>
+					<ul>
+						<li><a href="#">Pramuka</a></li>
+						<li><a href="#">Pencak Silat</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-2 col-md-6 single-footer-widget">
+					<h4>Info</h4>
+					<ul>
+						<li><a href="#">Pengumuman</a></li>
+						<li><a href="#">Berita</a></li>
+					</ul>
 				</div>
 			</div>
-		</footer>
+			<div class="row footer-bottom d-flex justify-content-between">
+				<p class="col-lg-8 col-sm-12 footer-text m-0 text-white">Copyright © 2018 All rights reserved | This template is
+					made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a></p>
+				<div class="col-lg-4 col-sm-12 footer-social">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-dribbble"></i></a>
+					<a href="#"><i class="fa fa-behance"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
 		<!--================ End footer Area  =================-->
 	
 		<!-- Optional JavaScript -->
