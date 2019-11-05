bbf70b690634be60ff1ca3ff1dab853f78c1152f
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-05 10:11:23 +0700

    Update pramuka.php

--- SMK/pramuka.php
+++ SMK/pramuka.php
@@ -231,25 +231,70 @@
 
 </div>
 				<!-- single course -->
-				
-				
-	<!--================ End Popular Courses Area =================-->
+<!--================ End Popular Courses Area =================-->
 
-	<!--================ Start Fact Area =================-->
-	
-	<!--================ End Fact Area =================-->
+<!--================ Start Fact Area =================-->
 
-	<!--================ Start Testimonial Area =================-->
-	
-	<!--================ End Testimonial Area =================-->
+<!--================ End Fact Area =================-->
+<!--================ Start Testimonial Area =================-->
 
-	<!--================ Start Registration Area =================-->
-	
-	<!--================ End Registration Area =================-->
+<!--================ End Testimonial Area =================-->
 
-	<!--================ Start Events Area =================-->
+<!--================ Start Registration Area =================-->
 
-    <!--================Contact Area =================-->
+<!--================ End Registration Area =================-->
+
+<!--================ Start Events Area =================-->
+<div class="whole-wrap">
+		<div class="container">
+			<div class="section-top-border">
+				<h3 class="title_color">Image Gallery</h3>
+				<div class="row gallery-item">
+					<div class="col-md-4">
+						<a href="img/elements/g1.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g1.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-4">
+						<a href="img/elements/g2.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g2.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-4">
+						<a href="img/elements/g3.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g3.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-6">
+						<a href="img/elements/g4.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g4.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-6">
+						<a href="img/elements/g5.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g5.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-4">
+						<a href="img/elements/g6.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g6.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-4">
+						<a href="img/elements/g7.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g7.jpg);"></div>
+						</a>
+					</div>
+					<div class="col-md-4">
+						<a href="img/elements/g8.jpg" class="img-gal">
+							<div class="single-gallery-image" style="background: url(img/elements/g8.jpg);"></div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!--================Contact Area =================-->
 
 	<!--================ Start footer Area  =================-->
 	<footer class="footer-area section_gap">
