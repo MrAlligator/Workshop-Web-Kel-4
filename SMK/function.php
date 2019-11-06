defa231400503e639b6b1f4a6ce8797dd93375cc
febreroaraya <febrero.fa@gmail.com>
2019-11-07 03:22:51 +0700

    Create function.php

---
+++ SMK/function.php
@@ -0,0 +1,14 @@
+<?php
+function salam(){
+    return "Selamat Datang, Admin!";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Function</title>
+</head>
+<body>
+        <h1><?= salam(); ?></h1>
+</body>
+</html>
