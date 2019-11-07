4eca0699ca8fd44b2d1de121dc9a878864d1ba1e
febreroaraya <febrero.fa@gmail.com>
2019-11-07 12:51:49 +0700

    Update admin.php

--- SMK/admin.php
+++ SMK/admin.php
@@ -46,7 +46,7 @@
 								aria-expanded="false">ADMIN</a>
 								<ul class="dropdown-menu">
 									<li class="nav-item"><a class="nav-link" href="#">Pengaturan</a></li>
-									<li class="nav-item"><a class="nav-link" href="#">Keluar</a></li>
+									<li class="nav-item"><a class="nav-link" href="logout.php">Keluar</a></li>
 								</ul>
 							</li>
 					</div>
