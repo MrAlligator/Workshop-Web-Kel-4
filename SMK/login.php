f41703995f6422d0bc5b05c6e3b93335181a86d1
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-07 08:06:26 +0700

    Update login.php

--- SMK/login.php
+++ SMK/login.php
@@ -22,26 +22,44 @@ if( isset($_POST["login"])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/logosmk.png" type="image/png">
+    <title>SMK Darus Salam</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>LOGIN</h1>
-
-<form action="" method="post">
-    <ul>
-        <li>
-            <label for="username">Username :</label>
-            <input type="text" name="username" id="username">
-        </li>
-        <li>
-            <label for="password">Password :</label>
-            <input type="password" name="password" id="password">
-        </li>
-        <li>
-            <button type="submit" name="login">LOGIN</button>
-        </li>
-    </ul>
-
-</form>
+    <div class="section-top-border">
+		<div class="row">
+			<div class="col-lg-4 col-sm-6">
+				<h1 class="mb-30 title_color">Login</h1>
+                <form action="" method="post">
+                    <ul>
+                        <li>
+                            <label for="username">Username :</label>
+                            <input type="text" name="username" id="username">
+                        </li>
+                        <li>
+                            <label for="password">Password :</label>
+                            <input type="password" name="password" id="password">
+                        </li>
+                        <li>
+                            <button type="submit" name="login">LOGIN</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
