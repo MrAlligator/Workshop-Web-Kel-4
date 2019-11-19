dce68188fa4e9fb68973703891068388cef9f107
febreroaraya <febrero.fa@gmail.com>
2019-11-19 11:50:20 +0700

    Update sejarah.php

--- SMK/sejarah.php
+++ SMK/sejarah.php
@@ -151,7 +151,7 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-6">
                         <div class="banner_content text-center">
-                            <h2>Sejarah</h2>
+                            <h2>SEJARAH SINGKAT</h2>
                         </div>
                     </div>
                 </div>
@@ -171,67 +171,15 @@
 	<!--================ Start Popular Courses Area =================-->
 	<section class="sample-text-area">
 		<div class="container">
-			<h3 class="text-heading title_color">Sejarah SMK Darus Salam Bondowoso</h3>
+			<h3 class="text-center">Sejarah SMK Darus Salam Bondowoso</h3>
 			<p style="font-family:Arial">         
-
 		</div>
     </section>
-    <div class="whole-wrap">
 		<div class="container">
-						<h3 class="mb-20 title_color">Dalam kategori multimedia content production menggunakan media sebagai berikut : </h3>
-						<div class="">
-							<ul class="unordered-list">
-								<li style="font-family:Arial"><p>Media tulisan (Teks)</li></p>
-								<li style="font-family:Arial"><p>Media Suara (Audio)</li></p>
-                                <li style="font-family:Arial"><p>Media Video</li></p>
-                                <li style="font-family:Arial"><p>Media Gambar (Grafik)</li></p>
-                                <li style="font-family:Arial"><p>Media Animasi</li>
-                                <li style="font-family:Arial"><p>Media Efek Khusus (Special Effect)</li>
-                                <li style="font-family:Arial"><p>Media Interaktif (Interactivity)</li>	
-							</ul>
-                    </div>
-</div>
-</div>
-<section class="sample-text-area">
-		<div class="container">
-			<h3 class="text-heading title_color">Multimedia ini tak akan jauh dari seni dan imajinasi kita. Menurut Hofstetter (2001) Multimedia terdiri dari 5 unsur, yaitu :</h3>
-            <p style="font-family:Arial"><b>Teks</b> merupakan unsur dasar untuk meyampaikan informasi. Dalam penempatan teks pun tidak sembarangan. Harus teliti mampu menemukan kunci empatik konsumen dengan imajinasi kita. Atau biasa dipelajari dalam materi Tipografi.</p></br>
-            <p style="font-family:Arial"><b>Gambar (Grafik)</b> merupakan informasi yang tidak dijelaskan dengan kata-kata. Kadangkala ketika kita di berkendara di jalan raya melihat baligo besar berisi informasi hanya dengan melihat gambar saja tanpa membaca kata-katanya kita sudah dapat menyimpulkan isi dari baligo tersebut.</p></br>
-            <p style="font-family:Arial"> <b>Audio</b> merupakan unsur yang bisa berupa percakapan, music atau efek suara. Multimedia tanpa audio akan terasa hambar seperti sayur tanpa garam. Hehehe … Format dasar audio ini biasanya WAVE dan MIDI.</p></br>
-            <p style="font-family:Arial"><b>Video</b> merupakan unsur yang membuat suatu informasi tampak terasa hidup dan lebih jelas. Tentu diiringi dengan audionya.</p></br>
-            <p style="font-family:Arial"><b>Animasi</b> merupakan simulasi gerakan yang dihasilkan  dengan menampilkan beberapa frame ke layer.</p>
-
-</div>
-<div class="whole-wrap">
-		<div class="container">
-       
-						<h3 class="mb-20 title_color">Dalam jurusan multimedia ini kita akan mempelajari hal – hal berikut :</h3>
-						<div class="">
-							<ul class="unordered-list">
-								<li style="font-family:Arial"><p>Dasar-dasar multimedia</li></p>
-								<li style="font-family:Arial"><p>Pengenalan dan Perakitan PC</li></p>
-                                <li style="font-family:Arial"><p>Etimologi Multimedia</li></p>
-                                <li style="font-family:Arial"><p>Fotografi  dan Teknik Pengambilan Gambar</li></p>
-                                <li style="font-family:Arial"><p>Belajar Animasi Dasar</li></p>
-                                <li style="font-family:Arial"><p>Alir Produksi Multimedia</li></p>
-                                <li style="font-family:Arial"><p>Pengelolaan Web</li></p>
-                                <li style="font-family:Arial"><p>Desain Animasi</li></p>
-                                <li style="font-family:Arial"><p>Video Shooting</li></p>
-                                <li style="font-family:Arial"><p>Audio dan Video Editing</li></p>
-                                <li style="font-family:Arial"><p>Menggambar Clean-up dan Sisip</li></p>
-                                <li style="font-family:Arial"><p>Perawatan peralatan multimedia</li></p>
-                                <li style="font-family:Arial"><p>Proposal pembuatan produk</li></p>
-                                <li style="font-family:Arial"><p>Seni Grafis dan Gambar 2D</li></p>
-                                <li style="font-family:Arial"><p>Stop Motion</li></p>
-                                <li style="font-family:Arial"><p>Efek Khusus</li></p>
-                                <li style="font-family:Arial"><p>Pembuatan Story Board</li></p>
-                                <li style="font-family:Arial"><p>Instalasi Sistem Operasi Dasar</li></p>
-                                <li style="font-family:Arial"><p>Final Project (Proyek Akhir pembuatan produk multimedia)</li></p>
-							</ul>
-						</div>
-                    </div>
-
-</div>
+            <p style="text-align:justify">Dalam upaya membangun masyarakat beragama, beradap dan berwawasan luas ditengah derasnya arus informasi komunikasi di era globalisasi, diperlukan kader-kader bangsa yang potensial untuk mendukung tercapainya kultur masyarakat ilmiah dan religi dalam proses pengembangan sumber daya manuasia. Wonosari merupakan salah satu kecamatan di Kabupaten Bondowoso yang masyarakatnya kurang mendapatkan ilmu pengetahuan agama khususnya di Dusun Jambi Asem dan sekitarnya, karena pada waktu itu pendidikan agama sangatlah minim.</p></br>
+			<p style="text-align:justify">Maka sejak saat itu, Pondok Pesantren Talang Mas Darus Salam didirikan pada tahun 2008, dengan bantuan para sesepuh dan warga masyarakat sekitar berusaha mendirikan pondok pesantren tersebut demi berlanjutnya generasi bangsa yang intelek dan religius khususnya dalam menegakkan ajaran agama Allah dan menjalankan ahlus sunnah waljamaah. Pondok Pesantren Talang Mas Darus Salam tetap berjalan hingga saat ini karena banyaknya dukungan dari para warga sekitar untuk tetap eksis dalam menjalankan pendidikan agama.</p></br>
+			<p style="text-align:justify">Pondok pesantren Talang Mas Darus Salam merupakan Pondok pesantren yang baru berdiri sejak tahun 2008 di bawah pengasuh K. Muhammad Kholis. Seiring berjalannya waktu, Pada tahun 2011 Barulah didirikan sebuah lembaga pendidikan Sekolah Menengah Kejuruan Darus Salam yang hingga sekarang telah meluluskan satu angkatan pertama sebanyak 17 siswa, sekitar 70% siswa tersebut bermukim di Pondok Pesantren Talang Mas Darus Salam. Pada tahun 2012 didirikanlah sebuah lembaga pendidikan yaitu Sekolah Menengah Pertama Darus Salam Untuk Menjaga Kualitas dan Kuantitas Pondok pesantren Talang Mas Darus Salam maka Kami jajaran Pengurus Pondok Pesantren Talang Mas Darus Salam mengajukan permohonan kepada Bapak Kepala Kantor Agama Kabupaten Bondowoso agar menjadi pondok pesantren yang memiliki status pengakuan dari pemerintah khususnya dilingkungan kementrian Agama Kabupaten Bondowoso.</p></br>
+		</div>
 				<!-- single course -->
 				
 				
