7459a87d5eb24a9c6daac2278da64bc429667e48
Rizkyakbar-mhs <55370807+Rizkyakbar-mhs@users.noreply.github.com>
2019-11-19 23:20:17 +0700

    profilesingkat done

--- SMK/profilsingkat.php
+++ SMK/profilsingkat.php
@@ -181,11 +181,11 @@
 						<h3 class="mb-20 title_color">Identitas Sekolah : </h3>
 						<div class="">
 							<ul class="unordered-list">
-								<li style="font-family:Arial"><p>Nama Sekolah	: </li></p>
-								<li style="font-family:Arial"><p>Alamat 		: </li></p>
-                                <li style="font-family:Arial"><p>Email			: </li></p>
-                                <li style="font-family:Arial"><p>NIS			: </li></p>
-                                <li style="font-family:Arial"><p>Berdiri pada   : </li></p>	
+								<li style="font-family:Arial"><p>Nama Sekolah	: SMK Darus Salam    </li></p>
+								<li style="font-family:Arial"><p>Alamat 		: Tangsil Wetan Rt.08 Rw. 03 </li></p>
+                                <li style="font-family:Arial"><p>Email			: darussalamsmk11@yahoo.com </li></p>
+                                <li style="font-family:Arial"><p>NIS			: 400390 </li></p>
+                                <li style="font-family:Arial"><p>Berdiri pada   : SMK Daru Salam Berdiri Mulai 2011</li></p>	
 							</ul>
                     	</div>
 		</div>
@@ -195,10 +195,10 @@
 						<h3 class="mb-20 title_color">Identitas Kepala Sekolah : </h3>
 						<div class="">
 							<ul class="unordered-list">
-								<li style="font-family:Arial"><p>Nama					: </li></p>
-								<li style="font-family:Arial"><p>Tanggal lahir 			: </li></p>
-                                <li style="font-family:Arial"><p>Pendidikan Terakhir	: </li></p>
-                                <li style="font-family:Arial"><p>Alamat Rumah			: </li></p>
+								<li style="font-family:Arial"><p>Nama					:  AHMAD MAHFUD, S. Pd </li></p>
+								<li style="font-family:Arial"><p>Tanggal lahir 			: Bondowoso, 23 Oktober 1992 </li></p>
+                                <li style="font-family:Arial"><p>Pendidikan Terakhir	: Sarjana (S-1) </li></p>
+                                <li style="font-family:Arial"><p>Alamat Rumah			: Sarjana (S-1) </li></p>
 							</ul>
                     	</div>
 		</div>
