5635f3c73a514c22de4cf66bc237d8112db514d6
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-21 08:32:44 +0700

    Update berita.php

--- SMK/berita.php
+++ SMK/berita.php
@@ -208,7 +208,7 @@
 				<div class="col-lg-3 col-md-6">
 					<div class="single_course">
                         <div class="course_head overlay">
-							<img class="img-fluid w-100" src="<?php echo "img/berita/".$d['foto']; ?>" alt="">
+							<img class="img-fluid w-100" src="<?php echo "admin/img/berita/".$d['foto']; ?>" alt="">
 						</div>
 						<div class="course_content">
 							<h4>
