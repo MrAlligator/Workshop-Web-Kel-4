d5b903d597f0d1573941a874ea290548c91974c0
febreroaraya <febrero.fa@gmail.com>
2019-11-21 18:52:52 +0700

    Update sambutan.php

--- SMK/sambutan.php
+++ SMK/sambutan.php
@@ -172,17 +172,17 @@
 	<!--================ Start Popular Courses Area =================-->
 	<section class="sample-text-area">
 		<div class="container">
-			<h3 class="text-center">Sambutan kepala sekolah</h3>
+			<h3 class="text-center">Sambutan Kepala Sekolah</h3>
 			<p style="font-family:Arial">         
 		</div>
     </section>
 		<div class="container">
         <p align="justify">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Alhamdulillahi robbil alamin kami panjatkan kehadlirat Allah SWT, bahwasannya dengan rahmat dan karunia-Nya lah 
 akhirnya Website & sistem informasi sekolah ini dapat kami publikasikan. Kami mengucapkan selamat datang di Website
-kami Sekolah Menengah Kejuruan Darus salam Bondowoso. yang saya tujukan untuk seluruh unsur pimpinan, guru, karyawan dan siswa 
+kami Sekolah Menengah Kejuruan Darus salam Bondowoso, yang saya tujukan untuk seluruh unsur pimpinan, guru, karyawan dan siswa 
 serta khalayak umum guna dapat mengakses seluruh informasi tentang segala profil, aktifitas/kegiatan serta fasilitas 
 sekolah kami. Kami selaku pimpinan sekolah mengucapkan terima kasih kepada tim pembuat Website ini yang telah berusaha 
-untuk dapat lebih memperkenal kan segala perihal yang dimiliki oleh sekolah. Dan tentunya Website & system informasi 
+untuk dapat lebih memperkenal kan segala perihal yang dimiliki oleh sekolah. Dan tentunya Website & Sistem informasi 
 sekolah kami masih terdapat banyak kekurangan, oleh karena itu kepada seluruh civitas akademika dan masyarakat umum 
 dapat memberikan saran dan kritik yang membangun demi kemajuan Website ini lebih lanjut.<br></p>
 <p align="justify">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Saya berharap Website ini dapat dijadikan wahana interaksi yang positif baik antar civitas akademika maupun masyarakat 
