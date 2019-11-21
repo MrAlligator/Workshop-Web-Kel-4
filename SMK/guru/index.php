46839cc3f14dd3e9397c750690b732386a473072
Ahmad Dandi Irawan <55469273+dandyIrawan@users.noreply.github.com>
2019-11-21 09:25:43 +0700

    update

--- SMK/guru/index.php
+++ SMK/guru/index.php
@@ -52,6 +52,7 @@
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								aria-expanded="false">Profil</a>
 								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="sambutan.php">Sambutan</a></li>
 									<li class="nav-item"><a class="nav-link" href="sejarah.php">Sejarah</a></li>
 									<li class="nav-item"><a class="nav-link" href="profilsingkat.php">Profil Singkat</a></li>
 									<li class="nav-item"><a class="nav-link" href="visimisi.php">Visi dan Misi</a></li>
