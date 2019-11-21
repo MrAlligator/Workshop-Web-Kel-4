c64c287ed5fd19e74c809b15a22466ebd06ec85c
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-21 08:31:58 +0700

    Update berita.php

--- SMK/berita.php
+++ SMK/berita.php
@@ -199,190 +199,34 @@
 			</div>
 			<div class="row">
 				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#"><i class="lnr lnr-user"></i>355</a>
-									</span>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a>
-									</span>
-								</div>
-								<div>
-									<span class="price">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<?php
+                    include 'koneksi.php';
+                    $data = mysqli_query($koneksi,"select * from tb_berita");
+                    while($d = mysqli_fetch_array($data)){
+                ?>
 				<!-- single course -->
 				<div class="col-lg-3 col-md-6">
 					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
+                        <div class="course_head overlay">
+							<img class="img-fluid w-100" src="<?php echo "img/berita/".$d['foto']; ?>" alt="">
 						</div>
 						<div class="course_content">
 							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
+								<a href="#"><?php echo $d['judul']?></a>
 							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-user"></i>355
-										</a>
-									</span>
-									<span class="meta_info"><a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a></span>
-								</div>
-								<div>
-									<span class="price df_color1">$150</span>
-								</div>
-							</div>
+							<p><?php echo $d['cuplikan']?></p>
 						</div>
 					</div>
-				</div>
-				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-user"></i>355
-										</a>
-									</span>
-									<span class="meta_info"><a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a></span>
-								</div>
-								<div>
-									<span class="price">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- single course -->
-				<div class="col-lg-3 col-md-6">
-					<div class="single_course">
-						<div class="course_head overlay">
-							<img class="img-fluid w-100" src="img/courses/trainer1.jpg" alt="">
-							<div class="authr_meta">
-								<img src="img/author1.png" alt="">
-								<span>Mart Taylor</span>
-							</div>
-						</div>
-						<div class="course_content">
-							<h4>
-								<a href="course-details.html">Learn React js beginners</a>
-							</h4>
-							<p>When television was young, there was a huge popular show based on the still popular fictional character of
-								Superman.</p>
-							<div class="course_meta d-flex justify-content-between">
-								<div>
-									<span class="meta_info">
-										<a href="#">
-											<i class="lnr lnr-user"></i>355
-										</a>
-									</span>
-									<span class="meta_info"><a href="#">
-											<i class="lnr lnr-bubble"></i>35
-										</a></span>
-								</div>
-								<div>
-									<span class="price df_color2">$150</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+                </div>
+                <?php
+                    }
+                ?>
 			</div>
 		</div>
 	</div>
 	<!--================ End Events Area =================-->
 
     <!--================Contact Area =================-->
-	<section class="contact_area section_gap">
-        <div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-6">
-					<div class="main_title">
-						<a href="#"><h2>Hubungi Kami</h2></a>
-					</div>
-				</div>
-			</div>
-            <div id="mapBox" class="mapBox" data-lat="-7.924831" data-lon="113.879707" data-zoom="17"
-                data-mlat="-7.924831" data-mlon="113.879707">
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="contact_info">
-                        <div class="info_item">
-                            <i class="lnr lnr-home"></i>
-                            <h6>Bondowoso, Jawa Timur</h6>
-                            <p>Santa monica bullevard</p>
-                        </div>
-                    </div>
-				</div>
-				<div class="col-md-4">
-					<div class="contact_info">
-						<div class="info_item">
-    	                    <i class="lnr lnr-phone-handset"></i>
-        	                <h6>00 (440) 9865 562</h6>
-            	            <p>Mon to Fri 9am to 6 pm</p>
-                	    </div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="contact_info">
-						<div class="info_item">
-                            <i class="lnr lnr-envelope"></i>
-                            <h6>support@colorlib.com</a></h6>
-                            <p>Send us your query anytime!</p>
-                        </div>
-					</div>
-				</div>
-			</div>
-        </div>
-    </section>
     <!--================Contact Area =================-->
 
 	<!--================ Start footer Area  =================-->
