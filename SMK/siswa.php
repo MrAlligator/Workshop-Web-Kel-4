39375449218f01c586b636e225ab043c631c56d2
febreroaraya <febrero.fa@gmail.com>
2019-11-21 14:21:42 +0700

    Update siswa.php

--- SMK/siswa.php
+++ SMK/siswa.php
@@ -170,7 +170,7 @@
 	<div class="whole-wrap">
 		<div class="container">
 			<div class="section-top-border">
-				<h3 class="mb-30 title_color">Data Siswa</h3>
+				<h3 class="mb-30 title_color text-center">Data Siswa</h3>
 				<div class="progress-table-wrap">
 					<div class="progress-table">
 						<div class="table-head">
