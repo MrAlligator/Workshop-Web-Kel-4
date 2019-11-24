8845d6d5b986fb9699b3c581dae35b665d9b15e3
Rizkyakbar-mhs <55370807+Rizkyakbar-mhs@users.noreply.github.com>
2019-11-24 10:34:01 +0700

    guru upload

---
+++ SMK/guru/fileupload.php
@@ -0,0 +1,26 @@
+<?php
+include 'koneksi.php';
+$nama = $_POST['nama'];
+$nip = $_POST['nip'];
+$jurusan = $_POST['jurusan'];
+$kelas = $_POST['kelas'];
+ $targetfolder = "pdf/";
+ $targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
+$b=$_FILES['file']['name'];
+$file_type=$_FILES['file']['type'];
+if ($file_type=="application/pdf") {
+ if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
+
+ {
+$query = mysqli_query($koneksi,"INSERT INTO `tb_materi` VALUES('','$nama','$nip','$jurusan','$kelas', '$b')");
+ echo "<script>alert('File $b $kelas berhasil di Upload!');location='inputmateri.php';</script>";
+ //Jalankan perintah insert ke database
+ }
+ else {
+ echo "<script>alert('File $b $kelas Gagal di Upload!');location='inputmateri.php';</script>";
+ }
+}
+else {
+ echo "<script>alert('Hanya Boleh upload PDF');location='inputmateri.php';</script>";
+}
+?>
