1e9b5a538dd075b7a576bdbd6545cc2263a8574f
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-11-29 01:19:20 +0700

    Update hapus-gambar.php

--- SMK/admin/hapus-gambar.php
+++ SMK/admin/hapus-gambar.php
@@ -3,11 +3,11 @@
 include 'koneksi.php';
  
 // menangkap data id yang di kirim dari url
-$id = $_GET['id'];
+$id = $_GET['id_foto'];
  
  
 // menghapus data dari database
-mysqli_query($koneksi,"delete from tb_foto where id='$id'");
+mysqli_query($koneksi,"delete from tb_foto where id_foto='$id'");
  
 // mengalihkan halaman kembali ke index.php
 header("location:photo.php");
