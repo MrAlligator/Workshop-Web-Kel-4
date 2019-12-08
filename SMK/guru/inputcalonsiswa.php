2be3ed4601f874c1cd75e3fafc2a8b7202235601
Rizkyakbar-mhs <55370807+Rizkyakbar-mhs@users.noreply.github.com>
2019-12-08 13:08:33 +0700

    calon siswa

---
+++ SMK/guru/inputcalonsiswa.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/logosmk.png" type="image/png">
+    <title>SMK Darus Salam</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!--================ Start Header Menu Area =================-->
+    <header class="header_area">
+		<div class="main_menu">
+			<div class="search_input" id="search_input_box">
+				<div class="container">
+					<form class="d-flex justify-content-between" method="" action="">
+						<input type="text" class="form-control" id="search_input" placeholder="Cari">
+						<button type="submit" class="btn"></button>
+						<span class="lnr lnr-cross" id="close_search" title="Tutup"></span>
+					</form>
+				</div>
+			</div>
+
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="index.php"><img src="img/logo2.png" alt=""></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav ml-auto">
+							<li class="nav-item"><a class="nav-link" href="../guru/aturdata.php">Kembali</a></li>
+							<li class="nav-item">
+								<a href="siswa2.php" class="nav-link" role="button" aria-haspopup="true"
+								aria-expanded="false">Daftar Siswa</a>
+							</li>
+							<li class="nav-item">
+								<a href="nilaisiswa.php" class="nav-link" role="button" aria-haspopup="true"
+								aria-expanded="false">Nilai Siswa</a>
+							</li>
+							<li class="nav-item">
+								<a href="uploadmateri.php" class="nav-link" role="button" aria-haspopup="true"
+								aria-expanded="false">Upload Materi</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link search" id="search">
+									<i class="lnr lnr-magnifier"></i>
+								</a>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">GURU</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+    <!--================ End Header Menu Area =================-->
+
+    <!--================Home Banner Area =================-->
+    
+    <!--================End Home Banner Area =================-->
+
+
+	<!-- Start Sample Area -->
+	<!-- End Sample Area -->
+	<!-- Start Button -->
+	<!-- End Button -->
+    <!-- Start Align Area -->
+    <div class="whole-wrap">
+		<div class="container">
+			<div class="section-top-border">
+				<div class="row">
+					<div class="col-lg-8 col-md-8">
+						<h3 class="mb-30 title_color">Input Calon Siswa</h3>
+						<form action="simpancalonsiswa.php" method="post" enctype="multipart/form-data">	
+							<table>
+								<tr>
+									<td>No Daftar</td>
+    	                            <td><input type="text" size="40" name="no_daftar" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Nama Lengkap</td>
+    	                            <td><input type="text" size="40" name="nama_lengkap" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Tempat Lahir</td>
+    	                            <td><input type="text" size="40" name="tmptlahir" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Tanggal Lahir</td>
+    	                            <td><input type="text" size="40" name="tgllahir" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Jenis Kelamin</td>
+    	                            <td><input type="text" size="40" name="jk" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Agama</td>
+    	                            <td><input type="text" size="40" name="agama" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Anak Ke</td>
+    	                            <td><input type="text" size="40" name="anak_ke" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Status Dalam Keluarga</td>
+    	                            <td><input type="text" size="40" name="status_dlm_keluarga" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Alamat Siswa</td>
+    	                            <td><input type="text" size="40" name="alamat_siswa" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Telepon Siswa</td>
+    	                            <td><input type="text" size="40" name="telp_siswa" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Sekolah Asal</td>
+    	                            <td><input type="text" size="40" name="sekolah_asal" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Alamat Sekolah</td>
+    	                            <td><input type="text" size="40" name="alamat_sekolah" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Tahun Lulus</td>
+    	                            <td><input type="text" size="40" name="tahun_lulus" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Total Nilai UN</td>
+    	                            <td><input type="text" size="40" name="total_nilai_un" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Nama Ayah</td>
+    	                            <td><input type="text" size="40" name="nama_ayah" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Nama Ibu</td>
+    	                            <td><input type="text" size="40" name="nama_ibu" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Alamat Orang Tua</td>
+    	                            <td><input type="text" size="40" name="alamat_ortu" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Telepon Orang Tua</td>
+    	                            <td><input type="text" size="40" name="telp_ortu" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Pekerjaan Ayah</td>
+    	                            <td><input type="text" size="40" name="perkerjaan_ayah" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Pekerjaan Ibu</td>
+    	                            <td><input type="text" size="40" name="pekerjaan_ibu" placeholder="" required class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Nama Wali</td>
+    	                            <td><input type="text" size="40" name="nama_wali" placeholder=""  class="single-input"></td>
+								</tr>
+								<tr>
+									<td>Alamat Wali</td>
+    	                            <td><input type="text" size="40" name="alamat_wali" placeholder=""  class="single-input"></td>
+								</tr>
+								<tr>
+					    			<td><input type="submit" value="Simpan"></td>
+								</tr>
+								<script>
+									function hanyaAngka(evt) {
+										var charCode = (evt.which) ? evt.which : event.keyCode
+										if (charCode > 31 && (charCode < 48 || charCode > 57))
+
+										return false;
+										return true;
+									}
+								</script>
+							</table>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Align Area -->
+
+	<!--================ Start footer Area  =================-->
+		<!--================ End footer Area  =================-->
+	
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/popper.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/stellar.js"></script>
+		<script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+		<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+		<script src="js/owl-carousel-thumb.min.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="vendors/counter-up/jquery.counterup.js"></script>
+		<script src="js/mail-script.js"></script>
+		<!--gmaps Js-->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+		<script src="js/gmaps.min.js"></script>
+		<script src="js/theme.js"></script>
+	</body>
+	
+	</html>
+	
---
+++ SMK/guru/tampilcalonsiswa.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/logosmk.png" type="image/png">
+    <title>SMK Darus Salam</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!--================ Start Header Menu Area =================-->
+    <header class="header_area">
+		<div class="main_menu">
+			<div class="search_input" id="search_input_box">
+				<div class="container">
+					<form class="d-flex justify-content-between" method="" action="">
+						<input type="text" class="form-control" id="search_input" placeholder="Cari">
+						<button type="submit" class="btn"></button>
+						<span class="lnr lnr-cross" id="close_search" title="Tutup"></span>
+					</form>
+				</div>
+			</div>
+
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="index.php"><img src="img/logo2.png" alt=""></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav ml-auto">
+						<li class="nav-item"><a class="nav-link" href="../admin/index.php">Home</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Profil</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/sambutan.php">Sambutan</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/sejarah.php">Sejarah</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/profilsingkat.php">Profil Singkat</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/visimisi.php">Visi dan Misi</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/struktur.php">Struktur</a></li>
+								</ul>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Data</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/siswa.php">Siswa</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/guru.php">Guru</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/karyawan.php">Karyawan</a></li>
+								</ul>
+							</li>
+							</li><li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Jurusan</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/multimedia.php">Multimedia</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Teknik Pemesinan</a></li>
+								</ul>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Ekstrakurikuler</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/pencak_silat.php">Pencak Silat</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/pramuka.php">Pramuka</a></li>
+								</ul>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Galeri</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/photo.php">Foto</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/video.php">Video</a></li>
+								</ul>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Info</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/pengumuman.php">Pengumuman</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/berita.php">Berita</a></li>
+								</ul>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Prestasi</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/akademik.php">Akademik</a></li>
+									<li class="nav-item"><a class="nav-link" href="../admin/nonakademik.php">Non - Akademik</a></li>
+								</ul>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link search" id="search">
+									<i class="lnr lnr-magnifier"></i>
+								</a>
+							</li>
+                            <li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">ADMIN</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="../admin/aturdata.php">Edit Data</a></li>
+									<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+    <!--================ End Header Menu Area =================-->
+
+    <!--================Home Banner Area =================-->
+    <section class="banner_area3">
+        <div class="banner_inner d-flex align-items-center">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <div class="banner_content text-center">
+                            <h2>CALON SISWA</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================End Home Banner Area =================-->
+
+
+	<!-- Start Sample Area -->
+	<!-- End Sample Area -->
+	<!-- Start Button -->
+	<!-- End Button -->
+	<!-- Start Align Area -->
+	<div class="whole-wrap">
+		<div class="container">
+			<div class="section-top-border">
+				<h3 class="mb-30 title_color text-center">Data Calon Siswa</h3>
+				<div class="progress-table-wrap">
+					<div class="progress-table">
+						<div class="table-head">
+							<div class="serial">No</div>
+							<div class="country">No Daftar</div>
+							<div class="visit">Nama Lengkap</div>
+							<div class="country">Tempat Lahir</div>
+							<div class="visit">Tanggal Lahir</div>
+							<div class="country">Jenis Kelamin</div>
+							<div class="country">Agama</div>
+							<div class="country">Aksi</div>
+
+
+							
+						</div>
+						<?php 
+							include 'koneksi.php';
+							$no = 1;
+							$data = mysqli_query($koneksi,"select * from tb_calon_siswa");
+							while($d = mysqli_fetch_array($data)){
+						?>
+						<div class="table-row">
+							<div class="serial"><?php echo $no++; ?></div>
+							<div class="country"><?php echo $d['no_daftar']; ?></div>
+							<div class="visit"><?php echo $d['nama_lengkap']; ?></div>
+							<div class="country"><?php echo $d['tmptlahir']; ?></div>
+							<div class="visit"><?php echo $d['tgllahir']; ?></div>
+							<div class="country"><?php echo $d['jk']; ?></div>
+							<div class="country"><?php echo $d['agama']; ?></div>
+							<div class="country"><a  href="info.php?id_daftar=<?php echo $d['id_daftar']; ?>">Info Detail</a></div>
+							
+						</div>
+						<?php
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Align Area -->
+
+	<!--================ Start footer Area  =================-->
+    <footer class="footer-area section_gap">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<font size="3" color="#333333">&copy;2019 || SMK DARUS SALAM<br>All Rights Reserved<br>Powered by Kelompok 4 | Design by <a href="http://instagram.com/febreroaraya_" target="newtab"><u>Febrero Araya K</u></a></font>
+			</div>
+		</div>
+	</footer>
+		<!--================ End footer Area  =================-->
+	
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/popper.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/stellar.js"></script>
+		<script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+		<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+		<script src="js/owl-carousel-thumb.min.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="vendors/counter-up/jquery.counterup.js"></script>
+		<script src="js/mail-script.js"></script>
+		<!--gmaps Js-->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+		<script src="js/gmaps.min.js"></script>
+		<script src="js/theme.js"></script>
+	</body>
+	
+	</html>
