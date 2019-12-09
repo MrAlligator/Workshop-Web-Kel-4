4703ebddc1798c704e91cd26755ba608dc0c15f8
Rizkyakbar-mhs <55370807+Rizkyakbar-mhs@users.noreply.github.com>
2019-12-09 08:53:14 +0700

    h

--- SMK/guru/tampilcalonsiswa.php
+++ SMK/guru/tampilcalonsiswa.php
@@ -47,7 +47,7 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-						<li class="nav-item"><a class="nav-link" href="../admin/index.php">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="../guru/index.php">Home</a></li>
 							<li class="nav-item submenu dropdown">
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								aria-expanded="false">Profil</a>
@@ -114,7 +114,7 @@
 							</li>
                             <li class="nav-item submenu dropdown">
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								aria-expanded="false">ADMIN</a>
+								aria-expanded="false">Guru</a>
 								<ul class="dropdown-menu">
 									<li class="nav-item"><a class="nav-link" href="../admin/aturdata.php">Edit Data</a></li>
 									<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
