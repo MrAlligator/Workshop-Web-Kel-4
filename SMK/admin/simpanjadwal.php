995c37f1aae03bc7d3584783ee19bad5e57af33e
Ahmad Dandi Irawan <55469273+dandyIrawan@users.noreply.github.com>
2019-12-26 12:52:23 +0700

    update sampek bisa

--- SMK/admin/simpanjadwal.php
+++ SMK/admin/simpanjadwal.php
@@ -7,10 +7,10 @@ $durasi = $_POST['durasi'];
 $hari = $_POST['hari'];
 
 
-if($nama_mapel=="" || $nama_guru=="" || $semester=="" || $hari=="" || $durasi==""){
+if($nama_mapel=="" || $semester=="" || $nama_guru=="" || $durasi=="" || $hari==""){
 	echo "<script>alert('data tidak lengkap!');location='tambah-jadwal.php';</script>";
 }else{
-$query = mysqli_query($koneksi,"INSERT INTO tb_jadwal VALUES('','$nama_mapel','$nama_guru','$semester', '$hari','$durasi')");
+$query = mysqli_query($koneksi,"INSERT INTO tb_jadwal VALUES('','$nama_mapel','$semester','$nama_guru', '$durasi','$hari')");
 if ($query){
 	echo "<script>alert('Berhasil Tersimpan!');location='jadwal.php';</script>";
 }else{
