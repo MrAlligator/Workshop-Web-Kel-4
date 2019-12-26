0a2db9c60dd5ce7f879e24bd77cc71463b8bbf9e
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2019-12-26 19:14:28 +0700

    Update hapus-karyawan.php

--- SMK/admin/hapus-karyawan.php
+++ SMK/admin/hapus-karyawan.php
@@ -7,6 +7,10 @@ $id = $_GET['id_guru'];
  
  
 // menghapus data dari database
+$pilih = mysqli_query($koneksi,"select * from tb_guru where id_guru='$id'");
+$data = mysqli_fetch_array($pilih);
+$foto = $data['foto_guru'];
+unlink($foto);
 $hapus = mysqli_query($koneksi, "delete from tb_guru where id_guru='$id'");
 if($hapus) { 
     echo '<script type="text/javascript">alert("Data Berhasil di Hapus.");document.location.href="../admin/karyawan2.php"</script>';
