8c20a64cca9021f46a877b725779af17a5955f60
Ahmad Dandi Irawan <55469273+dandyIrawan@users.noreply.github.com>
2019-12-30 04:30:52 +0700

    jadwal

--- SMK/admin/update-jadwal.php
+++ SMK/admin/update-jadwal.php
@@ -3,9 +3,8 @@
 include 'koneksi.php';
 
 // menangkap data yang di kirim dari form
-$id = $_POST['id'];
+$id = $_POST['id_jadwal'];
 $mapel = $_POST['nama_mapel'];
-$smstr = $_POST['semester'];
 $nama = $_POST['nama_guru'];
 $drsi = $_POST['durasi'];
 $hr = $_POST['hari'];
@@ -13,7 +12,7 @@ $hr = $_POST['hari'];
 
 
 // update data ke database
-mysqli_query($koneksi,"update tb_jadwal set nama_mapel='$mapel', semester='$smstr', nama_guru='$nama', durasi='$drsi', hari='$hr'");
+mysqli_query($koneksi,"update tb_jadwal set nama_mapel='$mapel', nama_guru='$nama', durasi='$drsi', hari='$hr', where id_jadwal ='$id'");
 
 // mengalihkan halaman kembali ke index.php
 header("location:jadwal.php");
