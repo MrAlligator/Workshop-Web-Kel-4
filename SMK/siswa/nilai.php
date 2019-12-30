532c9c59e1312f09df75cfb1cffcdc1a44f21033
febreroaraya <febrero.fa@gmail.com>
2019-12-30 10:50:37 +0700

    Update nilai.php

--- SMK/siswa/nilai.php
+++ SMK/siswa/nilai.php
@@ -168,3 +168,17 @@ if( ! isset($_SESSION['user'])){ // Jika tidak ada session username berarti dia 
 	</body>
 	
 	</html>
+	<script>
+		function hanyaAngka(evt) {
+			var charCode = (evt.which) ? evt.which : event.keyCode
+			if (charCode > 31 && (charCode < 48 || charCode > 57))
+			return false;
+			return true;
+		}
+		function hanyaHuruf(evt) {
+			var charCode = (evt.which) ? evt.which : event.keyCode
+			if ((charCode < 65 || charCode > 90)&&(charCode < 97 || charCode > 122)&&charCode>32)
+			return false;
+			return true;
+		}
+	</script>
